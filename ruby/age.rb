b8b032e49fbd2e9362fed5fc7f68df74849565d3
EC2 Default User <ec2-user@ip-172-31-0-143.ec2.internal>
2022-02-23 14:19:59 +0000

    add rubyfiles

---
+++ ruby/age.rb
@@ -0,0 +1,7 @@
+puts "sam is " + 27.to_s + " years old." 
+# 文字型に変換
+
+puts 100 + "200".to_i
+# 整数型に変換
+
+puts "my name is" + " menter Taro, " + 24.to_s + "years"
---
+++ ruby/weight.rb
@@ -0,0 +1,6 @@
+name = "A"
+weight = 50
+
+puts name + "'s weight "  +  weight.to_s + "kg."
+puts "#{name}'s weight is #{weight} kg."
+puts '#{name}s weight is #{weight} kg. '
